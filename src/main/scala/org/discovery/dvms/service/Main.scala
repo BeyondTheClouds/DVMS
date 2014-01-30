2de77ec6ce34bebcd0066558e2f885f908d150b7
Jonathan <jonathan.pastor@me.com>
2014-01-30 02:12:23 +0100

    fix for experimentation

--- src/main/scala/org/discovery/dvms/service/Main.scala
+++ src/main/scala/org/discovery/dvms/service/Main.scala
@@ -6,16 +6,17 @@ import org.discovery.AkkaArc.PeerActorProtocol.ConnectToThisPeerActor
 import akka.actor.{Props, ActorSystem}
 import org.discovery.dvms.DvmsSupervisor
 import org.discovery.dvms.factory.FakeDvmsFactory
+import org.discovery.AkkaArc.overlay.vivaldi.VivaldiServiceFactory
 
 object Main extends App {
 
 
   val system = ActorSystem(s"DvmsSystem", Configuration.generateLocalActorConfiguration)
 
-  val node1 = system.actorOf(Props(new DvmsSupervisor(FakeNetworkLocation(1), FakeDvmsFactory)))
-  val node2 = system.actorOf(Props(new DvmsSupervisor(FakeNetworkLocation(2), FakeDvmsFactory)))
-  val node3 = system.actorOf(Props(new DvmsSupervisor(FakeNetworkLocation(3), FakeDvmsFactory)))
-  val node4 = system.actorOf(Props(new DvmsSupervisor(FakeNetworkLocation(4), FakeDvmsFactory)))
+  val node1 = system.actorOf(Props(new DvmsSupervisor(FakeNetworkLocation(1), FakeDvmsFactory, VivaldiServiceFactory)))
+  val node2 = system.actorOf(Props(new DvmsSupervisor(FakeNetworkLocation(2), FakeDvmsFactory, VivaldiServiceFactory)))
+  val node3 = system.actorOf(Props(new DvmsSupervisor(FakeNetworkLocation(3), FakeDvmsFactory, VivaldiServiceFactory)))
+  val node4 = system.actorOf(Props(new DvmsSupervisor(FakeNetworkLocation(4), FakeDvmsFactory, VivaldiServiceFactory)))
 
   // create the links
   node2 ! ConnectToThisPeerActor(node1)
